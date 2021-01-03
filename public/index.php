fbf58e7e39703a8be24db92a8aa5eec8031bb398
subb98 <subb98@gmail.com>
2021-01-03 17:47:33 +0500

    Add Redis

--- public/index.php
+++ public/index.php
@@ -1,4 +1,9 @@
 <?php declare(strict_types=1);
 
 $dbh = new PDO('pgsql:host=postgres;dbname=example;port=5433', 'example', 'example');
-echo $dbh->query('SELECT NOW()')->fetchColumn();
+echo "PostgreSQL is running, current time: " . $dbh->query('SELECT NOW()')->fetchColumn() . "<br>";
+
+$redis = new Redis();
+$redis->connect('redis');
+$response = $redis->ping();
+echo "Redis is running: " . (1 === (int)$response ? 'PONG' : $response) . "<br>";
